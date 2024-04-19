daf02b55f6d3c59b4df420d7d4c280cf064d8693
slothgun059 <shaurya@mantena.in>
2024-04-19 12:07:14 +0530

    Update flextest.ino

--- arduino_code/flextest/flextest.ino
+++ arduino_code/flextest/flextest.ino
@@ -1,15 +1,13 @@
-int flexPin1 = A0;
-int flexPin2 = A1;
-int flexVal1;
-int flexVal2;
+int flexPin = A0;
+int flexVal;
+
 
 void setup() {
 Serial.begin(9600);
 }
 
 void loop() {
-flexVal1 = analogRead(flexPin1);
-flexVal2 = analogRead(flexPin2)
-Serial.println(flexVal1, flexVal2);
+flexVal = analogRead(flexPin);
+Serial.println(flexVal);
 delay(100);
 }
