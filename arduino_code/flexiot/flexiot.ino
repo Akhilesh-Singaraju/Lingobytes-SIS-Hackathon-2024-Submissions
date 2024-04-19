ce12e898b8244a2e523a4148d2c9c54a0f0f4672
slothgun059 <shaurya@mantena.in>
2024-04-19 15:16:21 +0530

    Blynk

--- arduino_code/flexiot/flexiot.ino
+++ arduino_code/flexiot/flexiot.ino
@@ -1,30 +1,31 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL3apmeeHsY"
-#define BLYNK_TEMPLATE_NAME "LingoBytesFinal"
-#define BLYNK_TEMPLATE_NAME "LingoBytesFinal"
-#define BLYNK_AUTH_TOKEN "fr9QZvP8gxwYKIc7AcU7cHoAgcKBnUxK"
+#define BLYNK_TEMPLATE_ID "TMPL3apmeeHsY" 
+#define BLYNK_TEMPLATE_NAME "LingoBytesFinal" 
+#define BLYNK_AUTH_TOKEN "fr9QZvP8gxwYKIc7AcU7cHoAgcKBnUxK" 
 
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp8266.h> 
 
-char ssid[] = "Guest";
-char pass[] = "477177";
+char ssid[] = "YourWiFiSSID"; 
+char pass[] = "YourWiFiPassword"; 
 
-int flexVal;
-BlynkTimer timer;
+int flexPin = A0; 
+int flexVal; 
 
-void myTimer(){
-  Blynk.virtualWrite(V0, flexVal);
+BlynkTimer timer; 
+
+void myTimer() {
+  flexVal = analogRead(flexPin); 
+  Blynk.virtualWrite(V1, flexVal); 
 }
 
 void setup() {
-Serial.begin(9600);
-Blynk.begin(fr9QZvP8gxwYKIc7AcU7cHoAgcKBnUxK, Guest, 477177);
-timer.setInterval(1000L, myTimer);
+  Serial.begin(9600);  
+  Blynk.begin(fr9QZvP8gxwYKIc7AcU7cHoAgcKBnUxK, ssid, pass); 
+  timer.setInterval(1000L, myTimer); 
 }
 
 void loop() {
-flexVal = analogRead(A0);
-Blynk.run();
-timer.run();
+  Blynk.run(); 
+  timer.run(); 
 }
