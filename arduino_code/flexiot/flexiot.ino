ba0e00927d546c7667e8aa4addffcd4741d0ba55
slothgun059 <shaurya@mantena.in>
2024-04-19 14:17:56 +0530

    Flex sensor

--- arduino_code/flexiot/flexiot.ino
+++ arduino_code/flexiot/flexiot.ino
@@ -10,11 +10,21 @@
 char ssid[] = "Guest";
 char pass[] = "477177";
 
+int flexVal;
+BlynkTimer timer;
+
+void myTimer(){
+  Blynk.virtualWrite(V0, flexVal);
+}
+
 void setup() {
 Serial.begin(9600);
 Blynk.begin(fr9QZvP8gxwYKIc7AcU7cHoAgcKBnUxK, Guest, 477177);
+timer.setInterval(1000L, myTimer);
 }
 
 void loop() {
+flexVal = analogRead(A0);
 Blynk.run();
+timer.run();
 }
